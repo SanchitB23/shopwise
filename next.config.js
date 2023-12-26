c5c9edccf7402df34feba6ec6c008672459e56ca
Sanchit Bhatnagar <dev.sanchitb23@gmail.com>
2023-12-26 22:17:29 +0530

    Generated build ID

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+    return 'Next_Build_ID';
+  },
+  distDir: 'dist/.next',
+};
 
 module.exports = nextConfig;
