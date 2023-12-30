1f54edefca7ae41614fe4c85c9be318ef56a173c
Sanchit Bhatnagar <dev.sanchitb23@gmail.com>
2023-12-30 11:34:07 +0530

    Pushed cloudinary-config.ts

---
+++ src/server/utils/cloudinary-config.ts
@@ -0,0 +1,15 @@
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env'),
+});
+
+const cloudinaryConfig = cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinaryConfig;
