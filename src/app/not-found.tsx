b4046f92b536abce7739b79ca8461aa3579643b7
Sanchit Bhatnagar <dev.sanchitb23@gmail.com>
2023-12-31 00:38:04 +0530

    Made Common Error Component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import NotFoundSvg from '@/resources/svg/not-found';
-import { Button } from '@/components/ui/button';
+import ErrorComponent from '@/components/layout/ErrorComponent';
 
 const NotFound = () => {
   return (
-    <div className={'container flex justify-center items-center h-full flex-col'}>
-      <NotFoundSvg />
-      <Button className={'my-10 animate-pulse'} variant={'secondary'}>
-        Return to Home
-      </Button>
-    </div>
+    <ErrorComponent
+      SvgComponent={NotFoundSvg}
+      isLink
+      linkText={'/'}
+      btnText={'page not found, return to home'}
+    />
   );
 };
 
--- src/client/components/layout/ErrorComponent.tsx
+++ src/client/components/layout/ErrorComponent.tsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 interface Props {
   className?: string;
   btnClassName?: string;
   btnText?: string;
-  onBtnClick: () => {};
+  onBtnClick?: () => void | {};
   SvgComponent: React.FC;
+  isLink?: boolean;
+  linkText?: string;
 }
+
 const ErrorComponent = ({
   className,
   SvgComponent,
-  onBtnClick,
+  onBtnClick = () => {},
   btnClassName,
-  btnText = 'Something went wrong, please Retry',
+  btnText = 'something went wrong, please retry',
+  isLink = false,
+  linkText,
 }: Props) => {
   return (
     <div className={cn('h-[80vh] flex justify-center items-center flex-col', className)}>
       <SvgComponent />
-      <Button
-        variant={'secondary'}
-        onClick={onBtnClick}
-        className={cn('my-10 animate-pulse', btnClassName)}>
-        {btnText}
-      </Button>
+      {isLink ? (
+        <Link href={linkText!}>
+          <Button
+            variant={'secondary'}
+            className={cn('my-10 animate-pulse capitalize', btnClassName)}>
+            {btnText}
+          </Button>
+        </Link>
+      ) : (
+        <Button
+          variant={'secondary'}
+          onClick={onBtnClick}
+          className={cn('my-10 animate-pulse capitalize', btnClassName)}>
+          {btnText}
+        </Button>
+      )}
     </div>
   );
 };
